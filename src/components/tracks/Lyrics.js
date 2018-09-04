4bc847ce5e7cc00ed081e72452691fe9f5a42d3a
Tharaka Romesh <tharakaromesh@gmail.com>
2018-09-04 08:59:54 +0530

    add moment and complete lyrics component

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -2,12 +2,17 @@ import React, { Component, Fragment } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Spinner from "../layout/Spinner";
+import Moment from "react-moment";
 
 class Lyrics extends Component {
   state = {
     track: {},
     lyrics: {}
   };
+
+  componentDidCatch(error, info) {
+    console.log(error, info);
+  }
 
   componentDidMount() {
     axios
@@ -35,19 +40,52 @@ class Lyrics extends Component {
   }
   render() {
     const { lyrics, track } = this.state;
+
     if (
       track === undefined ||
       lyrics === undefined ||
+      track.primary_genres === undefined ||
       (track === Object.keys(track).length) === 0 ||
-      (lyrics === Object.keys(lyrics).length) === 0
+      (lyrics === Object.keys(lyrics).length) === 0 ||
+      (track.primary_genres === Object.keys(track.primary_genres).length) === 0
     ) {
       return <Spinner />;
     } else {
+      console.log(track.primary_genres);
       return (
         <Fragment>
           <Link to="/" className="btn btn-dark btn-sm mb-4">
             Go Back
           </Link>
+          <div className="card">
+            <h5 className="card-header">
+              {track.track_name} by {""}
+              <span className="text-secondary">{track.artist_name}</span>
+            </h5>
+            <div className="card-body">
+              <p className="card-text">{lyrics.lyrics_body}</p>
+            </div>
+          </div>
+          <ul className="list-group mt-3">
+            <li className="list-group-item">
+              <strong>Album ID</strong>: {track.album_id}
+            </li>
+            <li className="list-group-item">
+              <strong>Song Genre</strong>:{" "}
+              {
+                track.primary_genres.music_genre_list[0].music_genre
+                  .music_genre_name
+              }
+            </li>
+            <li className="list-group-item">
+              <strong>Explicit Words</strong>:{" "}
+              {track.explicit === 0 ? "No" : "Yes"}
+            </li>
+            <li className="list-group-item">
+              <strong>Release Date</strong>:{" "}
+              <Moment format="MM/DD/YYYY">{track.first_release_date}</Moment>
+            </li>
+          </ul>
         </Fragment>
       );
     }
